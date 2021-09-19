cffc164263847f783644da6bf3e063cf8df5ad51
Nucker <nuckermail@gmail.com>
2021-09-19 07:50:40 +0100

    Fixed kick event not being cancnled in bungee

--- src/main/java/wtf/nucker/randomhub/bukkit/listeners/KickListener.java
+++ src/main/java/wtf/nucker/randomhub/bukkit/listeners/KickListener.java
@@ -1,10 +1,12 @@
 package wtf.nucker.randomhub.bukkit.listeners;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
 import wtf.nucker.randomhub.bukkit.utils.HubManager;
+import wtf.nucker.randomhub.bungee.RandomHub;
 
 /**
  * @author Nucker
@@ -16,9 +18,8 @@ public class KickListener implements Listener {
     @EventHandler
     public void onKick(PlayerKickEvent e) {
         Player player = e.getPlayer();
-        if(!e.getReason().equalsIgnoreCase("Server Closed")) return;
-        HubManager.connect(e.getPlayer());
-
+        if(!RandomHub.getInstance().getConfig().getStringList("kick-messages").contains(ChatColor.stripColor(e.getReason()))) return;
         e.setCancelled(true);
+        HubManager.connect(e.getPlayer());
     }
 }
--- src/main/java/wtf/nucker/randomhub/bungee/listeners/KickListener.java
+++ src/main/java/wtf/nucker/randomhub/bungee/listeners/KickListener.java
@@ -1,5 +1,6 @@
 package wtf.nucker.randomhub.bungee.listeners;
 
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -18,10 +19,10 @@ public class KickListener implements Listener {
 
     @EventHandler
     public void onServerKickEvent(ServerKickEvent e) {
-        ProxiedPlayer player = e.getPlayer();
         if(e.getKickedFrom() == null) return;
-        if(!e.getKickReason().equalsIgnoreCase("Server Closed")) return;
+        if(!RandomHub.getInstance().getConfig().getStringList("kick-messages").contains(ChatColor.stripColor(e.getKickReason()))) return;
 
+        e.setCancelled(true);
         HubManager.connect(e.getPlayer());
     }
 }
